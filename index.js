200d2829f380596e315cb2d3714b894f7d4e97d0
Didier <Kdondidier@gmail.com>
2021-11-17 20:16:18 +1100

    preparation for deployment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./startup/routes')(app)
 require('./startup/db')()
 require('./startup/config')()
 require('./startup/validation')()
+require('./startup/prod')()
 
 const port  = process.env.PORT || 3000
 const server = app.listen(port,()=>{console.log(`Listening on port ${port}`)})
