3d4852006697bf7134fc18dd928b5958d6022472
Didier <Kdondidier@gmail.com>
2021-11-17 20:52:54 +1100

    removed process.exit(1) on crash

--- startup/logging.js
+++ startup/logging.js
@@ -2,12 +2,12 @@ module.exports = function () {
   process.on("uncaughtException", (ex) => {
     // you can also logged this using a package like winston
     console.log("uncaught error during start up");
-    process.exit(1);
+    //process.exit(1);
   });
 
   process.on("unhandledRejection", (ex) => {
     // you can also logged this using a package like winston
     console.log("unhandled promise error");
-    process.exit(1);
+    //process.exit(1);
   });
 };
